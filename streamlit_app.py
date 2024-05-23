354a1f056b45a13eb6f08b1fabceca90ae464c97
woodywoodcock <stephen.woodcock@centene.com>
2024-05-23 06:38:54 -0500

    Update streamlit_app.py
    moving fruit request lookup under fruit chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,12 @@ if(ingredient_list):
     st.write(ingredient_list)
     st.text(ingredient_list)
     ingredients_string = ''
+    
 
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -42,6 +45,4 @@ if(ingredient_list):
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+"!", icon="✅")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
